66da0e3f54b5292c6e0e7a0a7223498b91440340
Davor Ocelic <docelic@crystallabs.io>
2018-08-14 00:16:25 +0200

    Document default option values

--- src/gum.cr
+++ src/gum.cr
@@ -130,23 +130,23 @@ module Gum
 			OptionParser.parse! do |parser|
 				parser.banner = "Usage: gum [arguments]"
 
-				parser.on("-r", "--remote", "Update remote branches before showing diffs") { @config.update_remote = true }
-				parser.on("-l", "--local", "Update local branches after showing diffs") { @config.update_local = true }
+				parser.on("-r", "--remote", "Update remote branches before showing diffs. Default true") { @config.update_remote = true }
 				parser.on("-R", "--no-remote", "Do not update remote branches before showing diffs") { @config.update_remote = false }
+				parser.on("-l", "--local", "Update local branches after showing diffs. Default false") { @config.update_local = true }
 				parser.on("-L", "--no-local", "Do not update local branches after showing diffs") { @config.update_local = false }
 
-				parser.on("-p", "--show-remote-pulls", "Show pulls from remote") { @config.show_remote_pulls = true }
+				parser.on("-p", "--show-remote-pulls", "Show pulls from remote. Default true") { @config.show_remote_pulls = true }
 				parser.on("-P", "--no-show-remote-pulls", "Do not show pulls from remote") { @config.show_remote_pulls = false }
-				parser.on("-u", "--show-local-pulls", "Show pulls from local") { @config.show_local_pulls = true }
+				parser.on("-u", "--show-local-pulls", "Show pull from local. Default false") { @config.show_local_pulls = true }
 				parser.on("-U", "--no-show-local-pulls", "Do not show pulls from local") { @config.show_local_pulls = false }
 
-				parser.on("-d", "--diffs", "Show diffs") { @config.show_diffs = true }
+				parser.on("-d", "--diffs", "Show diffs. Default true") { @config.show_diffs = true }
 				parser.on("-D", "--no-diffs", "Do not show diffs") { @config.show_diffs = false }
 
-				parser.on("-f", "--logfile FILE", "Log file (STDOUT, STDERR, or filename)") { |l| @config.logfile = l; set_logger }
-				parser.on("-s", "--loglevel LEVEL", "Log level (debug, info, warn, error, fatal, unknown)") { |l| @config.loglevel = l; set_logger }
+				parser.on("-f", "--logfile FILE", "Log file (STDOUT, STDERR, or filename). Default STDOUT") { |l| @config.logfile = l; set_logger }
+				parser.on("-s", "--loglevel LEVEL", "Log level (debug, info, warn, error, fatal, unknown). Default info") { |l| @config.loglevel = l; set_logger }
 
-				parser.on("-w", "--write-config", "Write final config to config file") { @config.write_config = true }
+				parser.on("-w", "--write-config", "Write final config to config file. Default false") { @config.write_config = true }
 
 				parser.on("-h", "--help", "Show this help") { puts parser; exit 0 }
 
